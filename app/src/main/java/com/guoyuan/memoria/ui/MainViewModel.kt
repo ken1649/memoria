1ce2009c045aa330ac17c7a4a3c71e8fb6855e0c
GuoYuan <ken101197@hotmail.com>
2026-06-19 15:33:52 +0800

    refactor: 在MainViewModel.kt中新增floatPreferencesKey導入

--- app/src/main/java/com/guoyuan/memoria/ui/MainViewModel.kt
+++ app/src/main/java/com/guoyuan/memoria/ui/MainViewModel.kt
@@ -24,7 +24,7 @@ import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import org.jsoup.Jsoup
-
+import androidx.datastore.preferences.core.floatPreferencesKey
 //
 val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 
